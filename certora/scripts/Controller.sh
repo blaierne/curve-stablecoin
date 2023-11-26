0aeced464a61da8cad075bf87166ff84bd21345c
Gad Elbaz <gad@certora.com>
2023-11-26 16:09:49 +0200

    link stableCoin

--- certora/scripts/Controller.sh
+++ certora/scripts/Controller.sh
@@ -1,10 +1,8 @@
-certoraRun contracts/Controller.vy \
+certoraRun contracts/Controller.vy certora/mocs/Stablecoin.vy \
     --verify Controller:certora/specs/Controller.spec \
+    --link Controller:STABLECOIN=Stablecoin \
     --loop_iter 3 \
     --optimistic_loop \
     --process evm \
     --wait_for_results \
-    --msg "Controller" --server production --coverage_info basic --debug --prover_version mike/CERT-3866-vyper-parse
-
-
-# certoraRun.py certora/configs/Sanity-Controller.conf --debug --server production --coverage_info basic --prover_version mike/CERT-3866-vyper-parse --loop_iter 5
+    --msg "Controller" --server production --coverage_info basic
