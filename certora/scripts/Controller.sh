008f59905d819293e23f0be89bc56165600d966c
Gad Elbaz <gad@certora.com>
2023-12-19 17:25:05 +0200

    update script

--- certora/scripts/Controller.sh
+++ certora/scripts/Controller.sh
@@ -4,10 +4,10 @@ certoraRun contracts/Controller.vy certora/mocs/Stablecoin.vy contracts/AMM.vy c
     --link Controller:STABLECOIN=Stablecoin \
     --link Controller:COLLATERAL_TOKEN=CollateralToken \
     --link Controller:AMM=AMM \
+    --link AMM:COLLATERAL_TOKEN=CollateralToken \
     --loop_iter 3 \
     --optimistic_loop \
     --process evm \
-    --wait_for_results \
     --rule_sanity \
-    --msg "Controller all" --server production --prover_version shelly/vyperlinking \
-    --prover_args '-tmpOptAllGhostsAreGlobal true -canonicalizeTAC false' 
+    --msg "Controller integrityOfLiquidate" --server production --prover_version shelly/vyperlinking \
+    --prover_args '-tmpOptAllGhostsAreGlobal true -canonicalizeTAC false'  --rule integrityOfLiquidate
